bc6200a023fddcf6816821948c2e74b92d4169b6
nrylord <61661058+nrylord@users.noreply.github.com>
2022-10-12 16:16:18 +0300

    created todo list D&D

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'vue-fullpage.js/dist/style.css'
 import VueFullPage from 'vue-fullpage.js'
 
+import UIComponents from '@/components/ui/index'
+
+
 const app = createApp(App)
+
+UIComponents.forEach(component => {
+    app.component(component.name, component)
+})
 
 app
     .use(VueFullPage)
